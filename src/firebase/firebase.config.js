ce100ac0ca25affcfe9408a14b3497365e945910
Ashraful Islam <ashrafulislam21211@gmail.com>
2022-11-24 13:44:02 +0600

    setup dne

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD0wb7nCTBfgH0OSwb5h3c9gkk9L1ly7UY",
+  authDomain: "readbook-28495.firebaseapp.com",
+  projectId: "readbook-28495",
+  storageBucket: "readbook-28495.appspot.com",
+  messagingSenderId: "127500231416",
+  appId: "1:127500231416:web:a0da68c7cf7a85ad6fd10d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
